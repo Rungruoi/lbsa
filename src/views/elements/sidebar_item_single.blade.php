299f19b4ca60217b5e2aa3dbeac54b3f178fad8c
iConcept <iconcept@MacBook-Pro.local>
2016-12-14 09:33:55 +0700

    Add element for sidebar

---
+++ src/views/elements/sidebar_item_single.blade.php
@@ -0,0 +1,5 @@
+<li class="@yield($id)">
+	<a href="{{ url($url) }}" title="{{ $title }}">
+		<i class="fa fa-lg fa-fw {{ $icon }}"></i> <span class="menu-item-parent">{{ $title }}</span>
+	</a>
+</li>
