47f28f063d6722c9ae4afc13268b2051c9b2300d
iConcept <iconcept@MacBook-Pro.local>
2016-12-16 16:13:25 +0700

    Add new file to example Add new view to multiple item

--- src/example/app.blade.php
+++ src/example/app.blade.php
@@ -6,4 +6,7 @@
 @include("layouts.elements.sidebar_item_single", ["title" => "Categories", "icon" => "fa fa-shopping-bag", "url" => "/category", "id" => "sidebar_category"])
 @include("layouts.elements.sidebar_item_single", ["title" => "Order", "icon" => "fa fa-shopping-bag", "url" => "/order", "id" => "sidebar_order"])
 @include("layouts.elements.sidebar_item_single", ["title" => "User", "icon" => "fa fa-user ", "url" => "/user", "id" => "sidebar_user"])
+@include("layouts.elements.sidebar_item_multi_open", ["title" => "User", "icon" => "fa fa-user ", "id" => "sidebar_user"])
+@include("layouts.elements.sidebar_item_single", ["title" => "User", "icon" => "fa fa-user ", "url" => "/user", "id" => "sidebar_user"])
+@include("layouts.elements.sidebar_item_close")
 @endpush
---
+++ src/views/elements/sidebar_item_multi_open.blade.php
@@ -0,0 +1,3 @@
+<li class="@yield($id)">
+	<a href="#"><i class="fa fa-lg fa-fw {{ $icom }}"></i> <span class="menu-item-parent">{{ $title }}</span></a>
+	<ul>
