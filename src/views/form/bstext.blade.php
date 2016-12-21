d461cef266a59ed06a1bae3c4e67b95d32855401
iConcept <iconcept@MacBook-Pro.local>
2016-12-21 11:50:45 +0700

    Add validation to textfield and textarea

--- src/views/form/bstext.blade.php
+++ src/views/form/bstext.blade.php
@@ -1,6 +1,11 @@
 <section>
-	{{ Form::label($title, null, ['class' => 'label']) }}
-	<label class="input">
+    <label class="label">{{ $title }}</label>
+    @if ($errors->has($name))
+	<label class="input state-error">
+    else
+	<label class="input state-error">
+    @endif
+	    
 	    <?php
 	    	$attrs = array('class' => 'form-control');
 			if (isset($place_holder))
@@ -13,10 +18,17 @@
 			}
 	    ?>
 	    {{ Form::text($name, $value, $attrs) }}
-	</label>
-	@if (isset($hin))
-		<div class="note">
-			{{ $hin }}
-		</div>
-	@endif
+
+    </label>
+
+    @if (isset($hin))
+	    <div class="note">
+	        {{ $hin }}
+	    </div>
+    @endif
+    @if ($errors->has($name))
+    	@foreach ($errors->get($name) as $error)
+			<div class="note note-error">{{ $error }}</div>
+		@endforeach
+    @endif
 </section>
--- src/views/form/bstextarea.blade.php
+++ src/views/form/bstextarea.blade.php
@@ -1,4 +1,18 @@
-<div class="form-group">
-	<label>{{ $title }}</label>
-	<textarea class="form-control" rows="3" placeholder="{{ @$placeholder }}" name="{{ $name }}">{{ $value }}</textarea>
-</div>
+<section>
+	<label class="label">{{ $title }}</label>
+    @if ($errors->has($name))
+	<label class="textarea textarea-resizable state-error">
+    else
+	<label class="textarea textarea-resizable state-error">
+    @endif								
+		<textarea rows="3" class="custom-scroll" placeholder="{{ @$placeholder }}" name="{{ $name }}">{{ $value }}</textarea> 
+	</label>
+	<!-- <div class="note">
+		<strong>Note:</strong> height of the textarea depends on the rows attribute.
+	</div> -->
+    @if ($errors->has($name))
+    	@foreach ($errors->get($name) as $error)
+			<div class="note note-error">{{ $error }}</div>
+		@endforeach
+    @endif
+</section>
