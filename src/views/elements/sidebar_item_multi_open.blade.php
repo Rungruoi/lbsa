0c3a155a85020852db44b40fac69c1c38a000f6c
iConcept <iconcept@MacBook-Pro.local>
2016-12-21 13:45:22 +0700

    Fix problem with multiple level sidebar

--- src/views/elements/sidebar_item_multi_open.blade.php
+++ src/views/elements/sidebar_item_multi_open.blade.php
@@ -1,3 +1,3 @@
 <li class="@yield($id)">
-	<a href="#"><i class="fa fa-lg fa-fw {{ $icom }}"></i> <span class="menu-item-parent">{{ $title }}</span></a>
+	<a href="#"><i class="fa fa-lg fa-fw {{ $icon }}"></i> <span class="menu-item-parent">{{ $title }}</span></a>
 	<ul>
