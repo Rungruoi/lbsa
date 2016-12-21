1aa735e0cebe44f1cff9be25ada893c50e6f7021
iConcept <iconcept@MacBook-Pro.local>
2016-12-21 13:01:27 +0700

    Select 2

--- src/views/form/bsselect2.blade.php
+++ src/views/form/bsselect2.blade.php
@@ -1,6 +1,10 @@
+<legend>
+	Select 2
+</legend>
+
 <div class="form-group">
-    {{ Form::label($title, null, ['class' => 'control-label']) }}
-	<select class="form-control  select2" name="{{ $name }}">
+	<label>{{ $title }}</label>
+	<select style="width:100%" class="select2" name="{{ $name }}">
     	@foreach ($items as $item)
     		<option value="{{ $item['value'] }}" {{ ($item['value'] == $value) ? "selected" : "" }}>{{ $item['name'] }}</option>
     	@endforeach
