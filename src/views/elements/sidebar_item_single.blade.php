e31a8c70f70173459a186d11b996ca373fadf557
iConcept <iconcept@MacBook-Pro.local>
2017-01-21 09:29:36 +0700

    Add child menu

--- src/views/elements/sidebar_item_single.blade.php
+++ src/views/elements/sidebar_item_single.blade.php
@@ -1,5 +1,10 @@
 <li class="@yield($id)">
 	<a href="{{ url($url) }}" title="{{ $title }}">
-		<i class="fa fa-lg fa-fw {{ $icon }}"></i> <span class="menu-item-parent">{{ $title }}</span>
+		<i class="fa fa-lg fa-fw {{ $icon }}"></i> 
+		@if (isset($is_child) && $is_child)
+		{{ $title }}
+		@else
+		<span class="menu-item-parent">{{ $title }}</span>
+		@endif
 	</a>
 </li>
