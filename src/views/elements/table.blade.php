2c7577f520d087f978d1981eb126f53bb3ebcd62
iConcept <iconcept@MacBook-Pro.local>
2017-01-31 00:16:52 +0700

    Add edit

--- src/views/elements/table.blade.php
+++ src/views/elements/table.blade.php
@@ -38,7 +38,7 @@
 
 
 @push('script')
-
+<script type="text/javascript" src="//editor.datatables.net/extensions/Editor/js/dataTables.editor.min.js"></script>
 <script type="text/javascript">
 $(document).ready(function() {
 
@@ -48,6 +48,18 @@ $(document).ready(function() {
 		tablet : 1024,
 		phone : 480
 	};
+
+	@if (isset($edit_url))
+	editor = new $.fn.dataTable.Editor( {
+        ajax: "{{ $edit_url }}",
+        table: "#{{ $special_id }}",
+    } );
+    @endif
+ 
+    // Activate an inline edit on click of a table cell
+    $('#example').on( 'click', 'tbody td:not(:first-child)', function (e) {
+        editor.inline( this );
+    } );
 
 	var t_{{ $special_id }} = $('#{{ $special_id }}').DataTable({
 		"sDom": "<'dt-toolbar'<'col-xs-12 col-sm-6 hidden-xs'f><'col-sm-6 col-xs-12 hidden-xs'<'toolbar'>>r>"+
