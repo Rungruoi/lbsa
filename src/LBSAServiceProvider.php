b6ac2e291173a71de37c3e143cd994c5493f715e
iConcept <iconcept@MacBook-Pro.local>
2017-03-10 09:35:53 +0700

    Quick fix id

--- src/LBSAServiceProvider.php
+++ src/LBSAServiceProvider.php
@@ -34,14 +34,14 @@ class LBSAServiceProvider extends ServiceProvider
             return "</div>";
         });
         Blade::directive('modal_open', function ($title, $id = "Default") {
-            return "<div class=\"modal fade\" id=\"$id\" tabindex=\"-1\" role=\"dialog\" aria-labelledby=\"myModalLabel\" aria-hidden=\"true\">
+            return "<div class=\"modal fade\" id=\"<?php echo $id; ?>\" tabindex=\"-1\" role=\"dialog\" aria-labelledby=\"myModalLabel\" aria-hidden=\"true\">
                         <div class=\"modal-dialog\">
                             <div class=\"modal-content\">
                                 <div class=\"modal-header\">
                                     <button type=\"button\" class=\"close\" data-dismiss=\"modal\" aria-hidden=\"true\">
                                         &times;
                                     </button>
-                                    <h4 class=\"modal-title\" id=\"myModalLabel\"><?php echo $title ?></h4>
+                                    <h4 class=\"modal-title\" id=\"myModalLabel\"><?php echo $title; ?></h4>
                                 </div>";
         });
         Blade::directive('modal_close', function ($title) {
