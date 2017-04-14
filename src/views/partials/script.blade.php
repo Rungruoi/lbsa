6a6340d89abcf7156f81eb996362876163341547
iConcept <iconcept@MacBook-Pro.local>
2017-04-14 11:31:58 +0700

    Downgrade to 2.0

--- src/views/partials/script.blade.php
+++ src/views/partials/script.blade.php
@@ -7,7 +7,7 @@
 <script data-pace-options='{ "restartOnRequestAfter": true }' src="{{ asset('/sa/js/plugin/pace/pace.min.js') }}"></script>
 
 <!-- Link to Google CDN's jQuery + jQueryUI; fall back to local -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
 <script>
     if (!window.jQuery) {
         document.write('<script src="{{ asset('/sa/js/libs/jquery-2.1.1.min.js') }}"><\/script>');
